c769a1399b7f972f743ded18162595ffddd36e1f
kienboeck <kienboec@technikum-wien.at>
2020-10-06 12:56:38 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # SuperMarioPlayer
+
 Real world problems mapped on the classic computer game Super Mario 1.
+
+Hereby we want to make complex decisions based on sensor data (similar to smart manufacturing problems).
+
+The environment is based on openAI/Gym ( see: https://github.com/Kautenja/gym-super-mario-bros )
+
+## Install
+
+* VS-Code
+* Python
+* pip install gym-super-mario-bros
+
+
+
+
+
+
+
---
+++ SuperMarioPlayer.py
@@ -0,0 +1,34 @@
+# https://github.com/Kautenja/nes-py
+from nes_py.wrappers import JoypadSpace;
+# https://github.com/Kautenja/gym-super-mario-bros
+import gym_super_mario_bros;
+
+
+COMPLEX_MOVEMENT = [
+    ['NOOP'],
+    ['right'],
+    ['right', 'A'],
+    ['right', 'B'],
+    ['right', 'A', 'B'],
+    ['A'],
+    ['left'],
+    ['left', 'A'],
+    ['left', 'B'],
+    ['left', 'A', 'B'],
+    ['down'],
+    ['up'],
+]
+
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = JoypadSpace(env, COMPLEX_MOVEMENT)
+
+done = True
+
+while True:
+    if done:
+        state = env.reset()
+
+    state, reward, done, info = env.step(4)
+    env.render()
+    
+env.close()
