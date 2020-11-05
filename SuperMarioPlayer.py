a1b27e69c0db91df041a5ed18fed1e103f105c29
Wolfgang Mair <wolfgang.mair912@gmail.com>
2020-11-05 11:27:26 +0100

    Added method that lets mario make a full high jump. Running through first level using only full jumps. No intelligence build.

--- SuperMarioPlayer.py
+++ SuperMarioPlayer.py
@@ -3,6 +3,9 @@ from nes_py.wrappers import JoypadSpace;
 # https://github.com/Kautenja/gym-super-mario-bros
 import gym_super_mario_bros;
 
+import time
+import random
+
 
 COMPLEX_MOVEMENT = [
     ['NOOP'],
@@ -19,16 +22,46 @@ COMPLEX_MOVEMENT = [
     ['up'],
 ]
 
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
+def BigJump(env, done, info):
+    height = 0
+    while True:
+        if height > info['y_pos']:
+            break;
+        if done:
+            state = env.reset()
+
+        height = info['y_pos']
+        state, reward, done, info = env.step(4)
+        env.render()
+        print("Jump!\n")
+
+    while True:
+        if height == info['y_pos']:
+            break;
+        if done:
+            state = env.reset()
+
+        height = info['y_pos']
+        state, reward, done, info = env.step(3)
+        env.render()
+        print("Wait!\n")
+    return state, reward, done, info
+
+env = gym_super_mario_bros.make('SuperMarioBros-v0').env
 env = JoypadSpace(env, COMPLEX_MOVEMENT)
 
 done = True
 
+state, reward, done, info = env.step(0)
+temp = state
+
 while True:
     if done:
         state = env.reset()
+        state, reward, done, info = env.step(0)
 
-    state, reward, done, info = env.step(4)
+    state, reward, done, info = BigJump(env, done, info)
     env.render()
-    
+    print(env.observation_space.high)
+    time.sleep(2)
 env.close()
