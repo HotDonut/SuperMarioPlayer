72ba58b88cb397b9b3171b4a52ac1d041aa776f0
Emmanuel Najfar <if20b200@technikum-wien.at>
2020-11-27 09:57:24 +0100

    Initial commit

--- SuperMarioPlayer.py
+++ SuperMarioPlayer.py
@@ -1,7 +1,7 @@
 # https://github.com/Kautenja/nes-py
-from nes_py.wrappers import JoypadSpace;
+from nes_py.wrappers import JoypadSpace
 # https://github.com/Kautenja/gym-super-mario-bros
-import gym_super_mario_bros;
+import gym_super_mario_bros
 
 
 COMPLEX_MOVEMENT = [
@@ -22,13 +22,26 @@ COMPLEX_MOVEMENT = [
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, COMPLEX_MOVEMENT)
 
+
+def jumpUpSuperHigh(highUpInTheSky):
+    i = 0
+    while i < highUpInTheSky:
+        state, reward, done, info = env.step(4)
+        env.render()
+        i += 1
+    state, reward, done, info = env.step(3)
+    env.render()
+    return state, reward, done, info
+
 done = True
 
 while True:
     if done:
         state = env.reset()
-
     state, reward, done, info = env.step(4)
     env.render()
-    
+    if info['life'] > 0:
+        state, reward, done, info = jumpUpSuperHigh(16)
+    else:
+        state, reward, done, info = jumpUpSuperHigh(13)
 env.close()
