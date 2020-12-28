154db39df73077c0dc7f29b3a2a65da52b2ca53a
Wolfgang Mair <wolfgang.mair912@gmail.com>
2020-12-28 03:08:10 +0100

    Rasterization and classes (messy Code)

--- SuperMarioPlayer.py
+++ SuperMarioPlayer.py
@@ -6,7 +6,88 @@ import gym_super_mario_bros;
 import time
 import random
 import numpy as np
-from os import system
+
+class Images():
+
+    def __init__(self):
+        #Goomba, boxes and floor have the same color
+        #cannot be used for value at goombas
+        self.goombaColor = np.array([228, 92, 16])
+
+        #Pits and background Sky
+        self.skyColor = np.array([104, 136, 252])
+
+        #Goomba Eye array
+        self.goombaEyeArray = np.array([[228, 92, 16], [228, 92, 16], [228, 92, 16],  [240, 208, 176], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [240, 208, 176], [228, 92, 16], [228, 92, 16], [228, 92, 16]])
+
+        #Pipe array
+        self.pipeArray = np.array([[0,0,0],[184,248,24],[184,248,24],[184,248,24],[0,168,0],[0,168,0],[184,248,24],[184,248,24],[184,248,24],[184,248,24],[184,248,24],[0,168,0],[184,248,24],[184,248,24]])
+
+        #Koopa array
+        self.koopaShellArray = np.array([[252, 252, 252], [0, 168, 0], [0, 168, 0], [0, 168, 0], [0, 168, 0], [252, 168, 68], [0, 168, 0], [252, 252, 252], [252, 252, 252], [252, 168, 68]])
+
+class Movement():
+    
+    def __init__(self):
+
+        self.sm_images = Images()
+
+        #Needed for better action distribution
+        #jumpright 25
+        #runright 10
+        #jumprunright 65
+        #else 0
+        self.basicWeights = [0,0,25,10,65,0,0,0,0,0,0,0,0,0]
+
+    def BigJump(self, env, reward, done, info):
+        height = 0
+
+        #print("Prepare!\n")
+        state, reward, done, info = env.step(3)
+        env.render()
+
+        while height <= info['y_pos']:
+
+            if done:
+                state = env.reset()
+
+            height = info['y_pos']
+            state, reward, done, info = env.step(4)
+        
+            env.render()
+            #print("Jump!\n")
+
+        while height != info['y_pos']:
+
+            if done:
+                state = env.reset()
+
+            height = info['y_pos']
+            state, reward, done, info = env.step(3)
+            env.render()
+            #print("Wait!\n")
+        return state, reward, done, info
+
+    def WeightedRandom(self, weightArray):
+
+        listOfValidActionsWithCountOfItemsInferedByWeights = []
+
+        for idx,weight in enumerate(weightArray): 
+            listOfValidActionsWithCountOfItemsInferedByWeights += [idx]*weight # inserts "weight"-times an action (= index of operation; see: COMPLEX_MOVEMENT)
+
+        return random.choice(listOfValidActionsWithCountOfItemsInferedByWeights)
+
+    def BadSearchMovement(self, state, reward, done, info, env):
+        maskGoomba = (state[194] == self.sm_images.goombaColor).all(axis = 1)
+        maskPit = (state[210] == self.sm_images.skyColor).all(axis = 1)
+
+        if np.any(maskGoomba):
+            return sm_movement.BigJump(env, reward, done, info)
+        else:
+            if np.any(maskPit):
+                return sm_movement.BigJump(env, reward, done, info)
+            else:
+                return env.step(sm_movement.WeightedRandom(sm_movement.basicWeights))
 
 
 COMPLEX_MOVEMENT = [
@@ -24,72 +105,43 @@ COMPLEX_MOVEMENT = [
     ['up'],
 ]
 
-def BigJump(env, reward, done, info):
-    height = 0
-
-    #print("Prepare!\n")
-    state, reward, done, info = env.step(3)
-    env.render()
-
-    while height <= info['y_pos']:
-
-        if done:
-            state = env.reset()
-
-        height = info['y_pos']
-        state, reward, done, info = env.step(4)
-        
-        env.render()
-        #print("Jump!\n")
-
-    while height != info['y_pos']:
-
-        if done:
-            state = env.reset()
-
-        height = info['y_pos']
-        state, reward, done, info = env.step(3)
-        env.render()
-        #print("Wait!\n")
-    return state, reward, done, info
-    
-def WeightedRandom(weightArray):
-
-    my_list = []
-
-    for i,j in enumerate(weightArray):
-        my_list += [i]*j
-
-    return random.choice(my_list)
-
-#Needed for better action distribution
-basicWeights = [0,0,25,10,65,0,0,0,0,0,0,0,0,0]
-
-#Goomba, boxes and floor have the same color
-goombaColor = np.array([228, 92, 16])
-
-#Pits and background Sky
-skyColor = np.array([104, 136, 252])
+sm_movement = Movement()
+sm_images = Images()
 
 env = gym_super_mario_bros.make('SuperMarioBros-v0').env
 env = JoypadSpace(env, COMPLEX_MOVEMENT)
 
 done = True
-state, reward, done, info = env.step(0)
-
 
 while True:
     if done:
         state = env.reset()
         state, reward, done, info = env.step(0)
 
+        # TODO create a string map based on info like eg:
+        # 
+        ############################################################
+        #                                                          # remove points / coin count / world / time
+        #                                                          #
+        #                                                          #
+        #                                                          #
+        #                                                          #
+        #                                                          #
+        #                                                          #
+        #                                                          #
+        #                  BBB                                     #
+        #                                                          #
+        #            M                 G                           #
+        #                                                          #
+        ############################################################
     
     #print(state.shape)
 
-    #state, reward, done, info = env.step(WeightedRandom(basicWeights))
-    #state, reward, done, info = BigJump(env, reward, done, info)
+    #state, reward, done, info = sm_movement.BadSearchMovement(state, reward, done, info, env)
+    #state, reward, done, info = env.step(sm_movement.WeightedRandom(sm_movement.basicWeights))
+    #state, reward, done, info = sm_movement.BigJump(env, reward, done, info)
     #state, reward, done, info = env.step(random.randint(0,len(COMPLEX_MOVEMENT)-1))
-    #state, reward, done, info = env.step(1)
+    state, reward, done, info = env.step(1)
     
     #for i in range(len(state[0])):
     #    state[192][i] = [0, 0, 0]
@@ -98,19 +150,47 @@ while True:
     #newColor = np.array([255, 255, 0])
     #for i in range(len(state)):
     #    for j in range(len(state[i])):
-    #        if np.all(state[i][j] == color):
+    #        if np.all(state[i][j] == goombaColor):
     #            state[i][j] = newColor
-
-    maskGoomba = (state[194] == goombaColor).all(axis = 1)
-    maskPit = (state[210] == skyColor).all(axis = 1)
     
-    if np.any(maskGoomba):
-        state, reward, done, info = BigJump(env, reward, done, info)
-    else:
-        if np.any(maskPit):
-            state, reward, done, info = BigJump(env, reward, done, info)
-        else:
-            state, reward, done, info = env.step(WeightedRandom(basicWeights))
+    
+    #Area of importance
+    #Height: 36-Border
+    #Width: 126-Border
+
+    
+
+    slicedState = state[30:, 120:]
+    #mask = np.zeros(slicedState.shape)
+    #state[35:, 130:] = mask
+
+    #Following Code finds Goombas eyes by slicing the array in 1:14:3 array pieces
+    #Seems to be buggy since it only finds the goomba exactly 3 times
+    """
+    #print(slicedState.shape[1])
+    for j in range(slicedState.shape[0]):
+        for i in range((int(slicedState.shape[1]/14))):
+            comparison = sm_images.goombaEyeArray == slicedState[1*j:1*(j+1),14*i:14*(i+1)]
+            exists = comparison.all()
+            if(exists):
+                print('j: '+ str(j) +', i: '+ str(i) + ', found = ' + str(exists))
+                print('Goomba-Array:')
+                print(sm_images.goombaEyeArray)
+                print('Sliced-Array:')
+                print(slicedState[1*j:1*(j+1),14*i:14*(i+1)])
+                print('#################################################################')
+                print('#################################################################')
+                time.sleep(5)
+    """
+
+    #Ideales Raster
+    #16x16
+    #Creating 16x16 fields that need to be tracked
+    color = [0,0,0]
+    for z in range(int(state.shape[0]/16)):
+        state[16*z, :] = color
+        for y in range(int(state.shape[1]/16)):
+            state[:, 16*y] = color
 
     env.render()
     time.sleep(0.02)
