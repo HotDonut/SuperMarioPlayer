2e0aed81cf0216a5a0c7a0318b2ede8be2387790
Wolfgang Mair <wolfgang_mair@hotmail.com>
2021-10-31 02:50:48 +0100

    Prepared and removed unused Code

---
+++ src/SuperMarioReinforcedLearning.py
@@ -0,0 +1,17 @@
+
+
+class SuperMarioReinforcedLearning():
+    def __inti__(self, theMap):
+        self.map = theMap
+
+    def saveNeuralNetwork(self):
+        pass
+
+    def loadNeuralNetwork(self):
+        pass
+
+    def deleteNeuralNetwork(self):
+        pass
+
+    def nextStep(self):
+        pass
